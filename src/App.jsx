66e3a0a47dd368490eac92e2b3a7aacefd09377b
vamsy6 <consumables.yandamuri@gmail.com>
2024-01-22 16:35:52 +0530

    Describe your changes here

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,16 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { PencilSVG, DeleteSVG } from "./icons";
+import { PencilSVG, DeleteSVG, AddSVG } from "./icons";
 import { ThemeProvider } from "./components/ui/theme-provider";
 import { ModeToggle } from "./components/ui/mode-toggle";
 import { Toaster, toast } from "sonner";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 function App() {
   return (
@@ -31,18 +37,22 @@ function App() {
           <ModeToggle />
         </div>
         <div className="container p-5 flex flex-col items-center md:flex-row md:justify-center">
-  <Input
-    className="w-3/12 mb-3.5 md:mr-3.5 md:mb-0"
+        <Input
+    className="w-3/5 md:w-2/3 mb-3.5 md:mr-3.5 md:mb-0 md:max-w-[300px]"
     type="text"
     placeholder="Enter your tasks here"
   />
 
-  <Button onClick={() => toast.success("A todo item has been added to your list")} type="submit">
-    Add
-  </Button>
-  <Toaster />
-</div>
-
+          <Button
+            onClick={() =>
+              toast.success("A todo item has been added to your list")
+            }
+            type="submit"
+          >
+            Add <AddSVG marginLeft={2} />
+          </Button>
+          <Toaster />
+        </div>
 
         <Table className=" w-3/20 mx-auto">
           <TableCaption>Number of tasks - 1</TableCaption>
@@ -64,9 +74,19 @@ function App() {
               </TableCell>
               <TableCell>
                 <DropdownMenu>
-                  <DropdownMenuTrigger className="font-black">
-                    ...
-                  </DropdownMenuTrigger>
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <DropdownMenuTrigger className="font-black">
+                          ...
+                        </DropdownMenuTrigger>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>More Options</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+
                   <DropdownMenuContent>
                     <DropdownMenuLabel>Actions</DropdownMenuLabel>
                     <DropdownMenuSeparator />
